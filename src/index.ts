4f74ac2e2906a60276f9c3904945f1b2fe145d25
zxyong <1203123788@qq.com>
2023-03-06 14:53:45 +0800

    不保存本地 使用bing的链接；readme展示最新31条；

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,29 @@
 import fetch from 'node-fetch'
-import {
-  writeFile,
-  existsSync,
-  writeFileSync,
-  readFileSync,
-  mkdirSync
-} from 'fs'
+import { existsSync, writeFileSync, readFileSync } from 'fs'
 
-const config = {
-  savePath: './static/',
+const option = {
   mapPath: './map.json',
   bingUrl: 'https://cn.bing.com',
+  // http://www.bing.com/HPImageArchive.aspx?idx=0&n=1&format=js&mkt=zh-CN&uhd=1&uhdwidth=1920&uhdheight=1080
   pictureURL: 'http://www.bing.com/HPImageArchive.aspx',
-  storyURL: 'http://cn.bing.com/cnhp/coverstory',
-  defaultQueries: {
+  queries: {
     idx: 0,
     n: 1,
-    format: 'js'
+    format: 'js',
+    mkt: 'zh-CN'
   }
 }
 
 main()
 
 // ;(async function () {
-//   config.defaultQueries.idx = 7
-//   config.defaultQueries.n = 8
+//   option.queries.idx = 7
+//   option.queries.n = 8
 //   await main()
-//   config.defaultQueries.idx = 0
-//   config.defaultQueries.n = 7
+//   option.queries.idx = 0
+//   option.queries.n = 7
 //   await main()
 // })()
-
 
 async function main() {
   try {
@@ -44,15 +37,10 @@ async function main() {
         enddate.slice(4, 6) +
         '-' +
         enddate.slice(-2)
-      const url_1080 = config.bingUrl + url
-      url = url.split('1920x1080').join('UHD')
-      const url_4k = config.bingUrl + url
-      const url_preview = config.bingUrl + url + '&w=480&h=270'
-      const name_preview = `${date}_preview.jpg`
-      const name_4k = `${date}_4k.jpg`
 
-      download(url_preview, name_preview)
-      download(url_4k, name_4k)
+      const url_1080 = option.bingUrl + url
+      const url_preview = url_1080.replace('1920', '480').replace('1080', '270')
+      const url_4k = url_1080.replace('1920', '3840').replace('1080', '2160')
 
       writeMap({
         hsh,
@@ -61,9 +49,7 @@ async function main() {
         copyright,
         url_preview,
         url_1080,
-        url_4k,
-        local_path_preview: config.savePath + name_preview,
-        local_path_4k: config.savePath + name_4k
+        url_4k
       })
     }
   } catch (e) {
@@ -74,10 +60,11 @@ async function main() {
 
 async function getBingPictures() {
   try {
-    const { idx, n, format } = config.defaultQueries
+    const { idx, n, format, mkt } = option.queries
     const URL =
-      config.pictureURL + `?idx=${idx}&n=${n}&format=${format}&mkt=zh-CN`
-  
+      option.pictureURL +
+      `?idx=${idx}&n=${n}&format=${format}&mkt=${mkt}&uhd=1&uhdwidth=1920&uhdheight=1080`
+
     console.log(`Request url: ${URL}`)
     const response = await fetch(URL)
     const data = await response.json()
@@ -91,34 +78,6 @@ async function getBingPictures() {
   }
 }
 
-/**
- * 保存图片
- * @param url 图片url
- * @param name 图片名称
- */
-async function download(url: string, name: string) {
-  if (!existsSync(config.savePath)) {
-    mkdirSync(config.savePath)
-  }
-
-  const path = config.savePath + name
-  if (existsSync(path)) {
-    console.log(`Warning: Duplicate image, not downloaded! → ${path}`)
-    return
-  }
-  const response = await fetch(url)
-  const arrayBuffer = await response.arrayBuffer()
-  const buffer = Buffer.from(arrayBuffer)
-
-  writeFile(path, buffer, err => {
-    if (err) {
-      console.log(err)
-      throw err
-    }
-    console.log(`Done: Download completed! Saved at ${path}`)
-  })
-}
-
 type PictureInfo = {
   hsh: string
   date: string
@@ -127,16 +86,14 @@ type PictureInfo = {
   url_preview: string
   url_1080: string
   url_4k: string
-  local_path_preview: string
-  local_path_4k: string
 }
 function writeMap(info: PictureInfo) {
   try {
-    if (!existsSync(config.mapPath)) {
-      writeFileSync(config.mapPath, '[]')
+    if (!existsSync(option.mapPath)) {
+      writeFileSync(option.mapPath, '[]')
     }
 
-    const buffer = readFileSync(config.mapPath)
+    const buffer = readFileSync(option.mapPath)
     const stringData = buffer.toString()
     const JSONData = JSON.parse(stringData) as PictureInfo[]
     const isRepeat = JSONData.some(v => v.hsh === info.hsh)
@@ -151,7 +108,7 @@ function writeMap(info: PictureInfo) {
       (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime() // 按日期从大到小排序
     )
 
-    writeFileSync(config.mapPath, JSON.stringify(JSONData))
+    writeFileSync(option.mapPath, JSON.stringify(JSONData))
     console.log('Done: Write map.json completed! ↓')
     console.log(info)
 
@@ -164,35 +121,40 @@ function writeMap(info: PictureInfo) {
 
 async function writeReadme(data: PictureInfo[]) {
   try {
-    const today = data.shift()
-    if (!today) {
+    if (!Array.isArray(data) || data.length === 0) {
       console.log('Error: WriteReadme: invalid data! ↓')
       console.log(data)
       return
     }
+    // 展示最新的31条
+    const writeData = data.slice(0, 31)
+    const today = writeData.shift()
 
-    const { date, title, local_path_4k } = today!
-    const dataList = [
+    const { date, title, url_4k } = today!
+    const tableList = [
       `# Bing Daily Wallpaper\n\n`,
       `### ${date} ${title}\n\n`,
-      `![](${local_path_4k})\n\n`,
+      `![](${url_4k})\n\n`,
       `|      |      |      |\n`,
       `|:----:|:----:|:----:|\n`
     ]
 
-    data.forEach((v, i) => {
-      const cell = `![](${v.local_path_preview})<br> ${v.date} [4K 版本](${v.local_path_4k}) <br> ${v.title}`
-      if ((i + 1) % 3 === 0) { // 一行3个
-        dataList.push(`|${cell}|\n`)
+    writeData.forEach((v, i) => {
+      const cell = `![](${v.url_preview})<br> ${v.date} [4K 版本](${v.url_4k}) <br> ${v.title}`
+      if ((i + 1) % 3 === 0) {
+        // 一行3个
+        tableList.push(`| ${cell} |\n`)
       } else {
-        dataList.push(`|${cell}`)
+        tableList.push(`| ${cell}`)
       }
     })
 
-    writeFileSync('./README.md', dataList.join(''))
+    writeFileSync('./README.md', tableList.join(''))
     console.log('Done: Write README.md completed!')
   } catch (e) {
     console.log(e)
     throw e
   }
 }
+
+function archive() {}
